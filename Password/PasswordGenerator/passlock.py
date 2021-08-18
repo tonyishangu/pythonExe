a3c8dc5959a387df6ffa8bbfd7c1942db7449656
tonyishangu <tonyishangu@gmail.com>
2021-08-18 14:55:00 +0300

    class credentials

--- Password/PasswordGenerator/passlock.py
+++ Password/PasswordGenerator/passlock.py
@@ -1,3 +1,4 @@
+from _typeshed import Self
 import random
 import string
 import pyperclip
@@ -18,3 +19,17 @@ class User:
 
     def delete_user(self):
         User.user_list.remove(self)
+
+class Credentials:
+    credentials = []
+
+    def __init__ (self, account,username,password):
+        self.account = account
+        self.username = username
+        self.password = password
+
+    def save (self):
+        Credentials.credentials.append(Self)
+
+    def delete (self):
+        Credentials.credentials.remove(self)
