74826e6f0688a1c998bdf04f86c3adb652055abb
tonyishangu <tonyishangu@gmail.com>
2021-08-19 15:16:52 +0300

    implement login and verification function

--- PasswordGenerator/index.py
+++ PasswordGenerator/index.py
@@ -1,3 +1,4 @@
+from Password.interface import check_credendtials
 from passlock import User, Credentials
 
 def create_new_user(username,password):
@@ -6,3 +7,11 @@ def create_new_user(username,password):
 
 def save(self):
     User.save_user()
+
+def display_user(self):
+    return User.dispaly_user()
+
+def login_user(username, password):
+
+    check_users=Credentials.verify_user(username, password)
+    return check_users
