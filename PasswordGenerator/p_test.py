e7a3547b9d035dc572344812cae5fdd9c057b073
tonyishangu <tonyishangu@gmail.com>
2021-08-20 15:35:11 +0300

    test for save user

--- PasswordGenerator/p_test.py
+++ PasswordGenerator/p_test.py
@@ -9,3 +9,11 @@ class TestClass(unittest.TestCase):
     def test_init(self):
         self.assertEqual(self.new_user.username,'Tonyishangu')
         self.assertEqual(self.new_user.password,'XyZ3thf1')
+
+    def test_save_user(self):
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+
+if __name__ == "__main__":
+    unittest.main()
