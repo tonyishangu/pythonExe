3f0667236274debd56adf24466543e73e4128c67
tonyishangu <tonyishangu@gmail.com>
2021-08-20 15:31:39 +0300

    intialize test

--- PasswordGenerator/p_test.py
+++ PasswordGenerator/p_test.py
@@ -5,3 +5,7 @@ class TestClass(unittest.TestCase):
 
     def setUp(self):
         self.new_user = User('Tonyishangu', 'XyZ3thf1')
+
+    def test_init(self):
+        self.assertEqual(self.new_user.username,'Tonyishangu')
+        self.assertEqual(self.new_user.password,'XyZ3thf1')
